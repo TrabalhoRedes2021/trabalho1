b035cb88b07c8974d8c95514d32e9133fb3d7279
Victor Hugo Dias Coelho <victorhdcoelho@gmail.com>
2021-08-26 13:13:54 -0300

    Add second port to servr

--- src/main.c
+++ src/main.c
@@ -15,17 +15,18 @@
 int main(int argc, char *argv[])
 {
   char * ip_server, * ip_client, * message;
-  int port_server, port_client;
+  int port_server, port_client, port_server2;
   ip_server = argv[1];
   port_server = atoi(argv[2]);
-  ip_client = argv[3];
-  port_client = atoi(argv[4]);
-  message = argv[5]; 
+  port_server2 = atoi(argv[3]);
+  ip_client = argv[4];
+  port_client = atoi(argv[5]);
+  message = argv[6]; 
   
   pid_t pid = fork();
   if(pid == 0)
   {
-    create_client(ip_server, port_server, ip_client, port_client, message);
+    create_client(ip_server, port_server2, ip_client, port_client, message);
   }
   create_server(ip_server, port_server, MAX_BUFFER);
   return 0; 
